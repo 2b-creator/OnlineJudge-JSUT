4f1547f894d4247c994514f09bb28119dcf45429
2b-creator <qjtykr65536@gmail.com>
2024-11-22 14:02:08 +0800

    string

--- Problems/ProblemOperator.py
+++ Problems/ProblemOperator.py
@@ -27,7 +27,7 @@ def get_question(start: int, num: int) -> dict[str, list]:
     res = cursor.fetchall()
     ls = []
     for i in res:
-        tp = tuple(i[0])
+        tp = eval(i[0])
         dic = {"title": tp[0], "tag": tp[1]}
         ls.append(dic)
     return {"data": ls}
