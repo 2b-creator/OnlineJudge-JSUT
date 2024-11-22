e658b9f9d7b8b21d3460cca5a9c1966b2f0c6483
2b-creator <qjtykr65536@gmail.com>
2024-11-22 13:57:09 +0800

    tag

--- Problems/ProblemOperator.py
+++ Problems/ProblemOperator.py
@@ -28,6 +28,7 @@ def get_question(start: int, num: int) -> dict[str, list]:
     ls = []
     for i in res:
         if len(i) < 2 or i[1] is None:  # 如果元组中没有足够的元素或 tag 为空
+            print(res)
             print(f"警告: 查询结果中的元组 {i} 不完整或 tag 为 NULL，跳过此行")
             continue
         dic = {"title": i[0], "tag": i[1]}
