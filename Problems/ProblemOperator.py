3d6f8464a1394d56b3cb2e9f2223bc32c8490b52
2b-creator <qjtykr65536@gmail.com>
2024-11-23 19:20:53 +0800

    dic

--- Problems/ProblemOperator.py
+++ Problems/ProblemOperator.py
@@ -22,13 +22,12 @@ def get_question(start: int, num: int) -> dict[str, list]:
     conn = psycopg2.connect(database=database_name, user=database_username, password=database_password, host=addr,
                             port=port)
     cursor = conn.cursor()
-    cursor.execute("SELECT (id, title, tag) FROM problems WHERE is_public = TRUE ORDER BY id LIMIT %s OFFSET %s;",
+    cursor.execute("SELECT id, title, tag FROM problems WHERE is_public = TRUE ORDER BY id LIMIT %s OFFSET %s;",
                    (num, start))
-    res: list[tuple[str]] = cursor.fetchall()
+    res = cursor.fetchall()
     ls = []
     for i in res:
-        tp = i[0][1:-1].split(",")
-        dic = {"id": tp[0], "title": tp[1], "tag": tp[2]}
+        dic = {"id": i[0], "title": i[1], "tag": i[2]}
         ls.append(dic)
     return {"datas": ls}
 
--- Scripts/ShellScripts/add_problem.sh
+++ Scripts/ShellScripts/add_problem.sh
@@ -1,14 +1,15 @@
 curl -X POST http://192.168.1.107:8000/api/add_problem \
--H "Content-Type: application/json" -H "access-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRpbSIsImV4cCI6MTczMjQyNzAzMn0.k3gi3ALt8lPz-OLQcQvP4sRoykedZzAbjm3wPV4He1g" \
+-H "Content-Type: application/json" \
+-H "access-token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6InRpbSIsImV4cCI6MTczMjQyNzAzMn0.k3gi3ALt8lPz-OLQcQvP4sRoykedZzAbjm3wPV4He1g" \
 -d '{
-  "title": "problem_2",
-  "problem_char_id": "multi",
-  "description": "multiply 2 nums",
-  "input_description": "a and b",
-  "output_description": "a*b",
+  "title": "JSUT-OJ 试机题",
+  "problem_char_id": "addTwoNum",
+  "description": "不要笑！这题见证了 JSUT-OJ 项目的开始，也是作为一个 oier 梦的起点。请你求出输入两个数的和，空格隔开。",
+  "input_description": "输入只有一行，包括 $2$ 个整数 $a,b$",
+  "output_description": "输出一个数字，是为 $a+b$ 的求和结果",
   "sample_input": "3 3",
-  "tag": "test",
-  "sample_output": "9",
+  "tag": "测试",
+  "sample_output": "6",
   "difficulty": 1,
   "time_limit": 1,
   "memory_limit": 128
